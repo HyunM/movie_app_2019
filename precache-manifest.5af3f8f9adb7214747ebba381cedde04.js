1f777818067c04e974e9bb4ab68a87b83c2fa1a2
HyunM <wisek0226@gmail.com>
2020-06-27 17:20:41 -0700

    Updates

---
+++ precache-manifest.5af3f8f9adb7214747ebba381cedde04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3b7da3c2ecc244dc9463461ce6ab50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116805428fcb42ad3fe2",
+    "url": "/static/css/main.f3822d75.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116805428fcb42ad3fe2",
+    "url": "/static/js/main.0e15e495.chunk.js"
+  },
+  {
+    "revision": "50b67832ef5f1dc086fe",
+    "url": "/static/js/runtime-main.b486f117.js"
+  }
+]);

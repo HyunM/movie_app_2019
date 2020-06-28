13568fb25d71f94ce42417cde44a9ca12001fe19
HyunM <wisek0226@gmail.com>
2020-06-27 18:44:24 -0700

    Router, Navigation, Sharing Props Between Routes, Redirecting

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navigation.css";
+
+import { Link } from "react-router-dom";
+
+function Navigation(){
+    return(
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to={{
+            pathname:'/about',
+            state:{
+                fromNavigation: true
+            }
+        }}>About</Link>
+    </div>
+    );
+}
+
+export default Navigation;
